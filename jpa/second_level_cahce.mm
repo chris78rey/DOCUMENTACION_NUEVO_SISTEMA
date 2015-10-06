b1c261ed350c57298efa3d1d839e219be5eb0e28
chris78rey <cris_78_rey@hotmail.com>
2015-10-06 11:21:35 -0500

    SE VA A DOCUMENTAR COMO PARAR LA BASE ACTUAL AL RAC
    SE VA A DOCUMENTAR COMO PARAR LA BASE ACTUAL AL RAC

---
+++ jpa/second_level_cahce.mm
@@ -0,0 +1,52 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1443627781216" ID="ID_228067934" MODIFIED="1443627856485" TEXT="SECOND LEVEL CACHE">
+<node CREATED="1443627877311" ID="ID_123283937" MODIFIED="1443627879196" POSITION="right" TEXT="Change the second-level cache to one of the cache mode settings "/>
+<node CREATED="1443627879770" ID="ID_1459472580" MODIFIED="1443627903014" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Control which entities may be cached (see Controlling whether Entities May Be Cached)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1443627915577" ID="ID_1909566352" MODIFIED="1443627917253" POSITION="right" TEXT="Change the cache&apos;s retrieval or store modes (see Setting the Cache Retrieval and Store Modes) ">
+<node CREATED="1443628394523" ID="ID_187915541" MODIFIED="1443628486553">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      javax.persistence.retrieveMode
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1443628512061" ID="ID_1850734717" MODIFIED="1443628513824" TEXT="USE"/>
+<node CREATED="1443628514219" ID="ID_1723663911" MODIFIED="1443628517632" TEXT="BYPASS"/>
+</node>
+<node CREATED="1443628407819" ID="ID_1964102204" MODIFIED="1443628472855">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      javax.persistence.storeMode
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1443628555103" ID="ID_2323531" MODIFIED="1443628556810" TEXT="USE"/>
+<node CREATED="1443628557981" ID="ID_319106834" MODIFIED="1443628559922" TEXT="BYPASS"/>
+<node CREATED="1443628560317" ID="ID_1994440587" MODIFIED="1443628564330" TEXT="REFRESH"/>
+</node>
+</node>
+</node>
+</map>
